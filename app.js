a59946ce3f3710a035e6b899bd781dc4078f9831
rollfokcus <rollfokcus@rollfokcuss-MacBook-Pro.local>
2016-03-31 00:01:04 +0800

    #add jade test

--- app.js
+++ app.js
@@ -9,9 +9,14 @@ app.set("views", "./views");
 app.set("view engine", "jade");
 
 app.use(function(request, response, next) {
+        console.log("receive a request");
         //使用模板引擎渲染，data为渲染的对象数据
         var data = {};
+        data = {"book": {"name": "Hello", "price": 12.99}};
         response.render("test.jade", data);
+
+        //response.end("do you reveice??");
 });
 
 app.listen(1024);
+console.log('you can visit caoduozi in http://127.0.0.1:1024/');
